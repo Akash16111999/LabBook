105a9498f6db1951acd4b6e4340076d9e5a6e05f
Akash16111999 <84120813+Akash16111999@users.noreply.github.com>
2021-05-24 18:26:51 +0530

    Delete lab4_library directory

--- lab4_library/Book.java
+++
@@ -1,8 +0,0 @@
-package lab4_library;
-
-public class Book extends WrittenItem{
-	public Book(int identificationNumber, String title, int numberOfCopies, String author) {
-		super(identificationNumber,title,numberOfCopies,author);
-	}
-	
-}
